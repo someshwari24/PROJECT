f23bd8493f0b3ede6154707873d8b4e97579a98b
someshwari24 <someshwari2418@gmail.com>
2025-03-16 19:15:15 +0530

    Initial commit

--- server.js
+++ server.js
@@ -17,11 +17,14 @@ app.use(bodyParser.json());
 
 // MySQL Database Connection
 const db = mysql.createConnection({
+  connectionLimit: 10, // Adjust based on needs
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  queueLimit: 0
 });
 
 db.connect(err => {
