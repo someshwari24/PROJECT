98876ee0e8ce85f2824397ee15f597b34252e240
someshwari24 <someshwari2418@gmail.com>
2025-03-16 16:25:32 +0530

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
 const validator = require('validator');
 const cors = require('cors');
